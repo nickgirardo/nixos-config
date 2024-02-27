34251c7abbf7d69c65b5d6f0c0f6a740efc600b0
Nick Girardo <nickgirardo@gmail.com>
2024-02-27 18:52:53 -0500

    Home Manager

--- flake.nix
+++ flake.nix
@@ -3,19 +3,25 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, home-manager, ... }:
   let
     lib = nixpkgs.lib;
     system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
-    nixosConfigurations = {
-      nixos = lib.nixosSystem {
-        inherit system;
-        modules = [ ./configuration.nix ];
-      };
+    nixosConfigurations.nixos = lib.nixosSystem {
+      inherit system;
+      modules = [ ./configuration.nix ];
     };
-
+    homeConfigurations.nick = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [ ./home.nix ];
+    };
   };
 }
