3a8858a87fbcb4d9dffb5499fa86127dae24bc8f
Nick Girardo <nickgirardo@gmail.com>
2024-07-11 19:31:29 -0400

    Bash PS1

--- configuration.nix
+++ configuration.nix
@@ -118,6 +118,9 @@
     wget
   ];
 
+
+  programs.bash.promptInit = ''PS1="\\$ "'';
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
