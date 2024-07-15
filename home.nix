e26d80247d1b8dd2001d619ec660418aec57ee45
Nick Girardo <nickgirardo@gmail.com>
2024-07-15 17:51:05 -0400

    Install chromium and slack

--- home.nix
+++ home.nix
@@ -34,7 +34,11 @@
 
     pkgs.thunderbird
 
+    pkgs.chromium
+
     pkgs.discord
+
+    pkgs.slack
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
