cefc973664b53fbfd650691dcfc23cb96fd81839
Nick Girardo <nickgirardo@gmail.com>
2024-09-19 01:10:25 -0400

    Add aws cli

--- home.nix
+++ home.nix
@@ -39,6 +39,8 @@
     pkgs.discord
 
     pkgs.slack
+
+    pkgs.awscli2
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
